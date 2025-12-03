84d3b183c00adbfc3e9be39273e73405ec03c1fe
Jcicenas <cicenas.4@osu.edu>
2025-12-03 14:36:52 -0500

    test

--- main.cpp
+++ main.cpp
@@ -10,19 +10,22 @@
 #include <fstream>
 #include <iostream>
 #include <FEHImages.h>
+#include <cmath>
 using namespace std;
+
+
 class target{
     private:
     //init variables for money, pwd, and bitstrength
     int money;
-    char pwd[16];
+    string pwd;
     float bitStrength;
     
     public:
     //set up basic functions to get private values along with constructor
     target();
-    int getMoney();
-    // int function to get string
+    float getMoney();
+    string getPass();
     float getBitStrength();  
 
 };
@@ -37,10 +40,17 @@ class player{
     player();
     int getMoney();
     float getHashpower();
-    float* getHardware();
+    int* getHardware();
 };
+//function declarations
+string generatePass();
+int generateRandInt();
+float getBitsEntropy(string password);
+float generateRandFloat();
+//main
 int main(int argc, char const *argv[])
 {
+
     //credits top left 9, 212 bottom 9, 230 77,212 77,230
     //play button 211 - 238 y 128 - 193 x
     //help 254 - 294 x 212 - 230 y
@@ -75,8 +85,8 @@ int main(int argc, char const *argv[])
 }
 
 
-//function to generate a random number given a range of nums
-int rand(int min, int max){
+//function to generate a random int given a range of ints
+int generateRandInt(int min, int max){
     std::random_device randseed;
     std::mt19937 randengine(randseed());
     std::uniform_int_distribution<int> distribution(min, max);
@@ -84,8 +94,16 @@ int rand(int min, int max){
     return randomNum;
 
 }
+//function to generate a random float given a range of floats
+float generateRandFloat(float min, float max){
+    std::random_device randseed;
+    std::mt19937 randengine(randseed());
+    std::uniform_int_distribution<float> distribution(min, max);
+    int randomNum = distribution(randengine);
+    return randomNum;
 
-target::target(){
+}
+string generatePassword(){
     //randomized password - bit strength will be based off of it
     /*  first randomize type str
     1 - common word
@@ -95,6 +113,116 @@ target::target(){
     5 - uppcase nums & symbols
     randomize length - 4 or longer for 2-5
     */
+   string pass, charArr = "abcdefghijklmnopqrstuvwxyz";
+   int randCase = generateRandInt(1, 5);
+   int pwdLength = generateRandInt(4, 20);
+   ifstream ifFile;
+   switch (randCase)
+   {
+    case 1:
+        //case 1 - common word from database
+        ifFile.open("commonwords.txt");
+        int tempindex = generateRandInt(1,9999);
+        for (int i = 0; i < tempindex; i++)
+        {
+            getline(ifFile, pass);
+        }
+        ifFile.close();
+        return pass;
+        break;
+    case 2:
+        //case 2 - just lowercase
+        pass.reserve(pwdLength);
+        for (int i = 0; i < pwdLength; i++)
+        {
+            pass += charArr[generateRandInt(0, charArr.size())];
+        }
+        return pass;
+        break;
+    case 3:
+        //case 3 - lowercase and ints
+        charArr += "1234567890";
+        pass.reserve(pwdLength);
+        for (int i = 0; i < pwdLength; i++)
+        {
+            pass += charArr[generateRandInt(0, charArr.size())];
+        }
+        return pass;
+        break;
+    case 4:
+        //case 4 - lowercase, uppercase, and ints
+        charArr += "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        pass.reserve(pwdLength);
+        for (int i = 0; i < pwdLength; i++)
+        {
+            pass += charArr[generateRandInt(0, charArr.size())];
+        }
+        return pass;
+        break;
+    case 5:
+        //lowercase, uppercase, and ints
+        charArr += "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
+        pass.reserve(pwdLength);
+        for (int i = 0; i < pwdLength; i++)
+        {
+            pass += charArr[generateRandInt(0, charArr.size())];
+        }
+        return pass;
+        break;
+    default:
+        break;
+   }
+}
+/*function to get bit strength (will be able to calculate time based off this)
+    formula for measuring bits of entropy - E = L * log(base2)R
+    source and more information: https://proton.me/blog/what-is-password-entropy
+*/
+float getBitsEntropy(string password){
+    //store password length and initialize var for possible num of characters
+    int length = password.length();
+    int possibleChars = 26;
+    //add to characters if any of these are found
+    if(password.find_first_of("1234567890") != string::npos){
+        possibleChars += 10;
+    }
+    if(password.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") != string::npos){
+        possibleChars += 26;
+    }
+    if(password.find_first_of("!@#$%^&*") != string::npos){
+        possibleChars += 8;
+    }
+    //compute bits
+    return length * log2(possibleChars);
+}
+target::target(){
+    //construct each target objects random values
+    string temp = generatePassword();
+    pwd = temp;
+    bitStrength = getBitsEntropy(temp);
+    money = 1000 * (pow(bitStrength, generateRandFloat(1.0, 2.0)));
     
+}
+//getter functions for player class variables
+string target::getPass(){
+    return pwd;
+}
+float target::getBitStrength(){
+    return bitStrength;
+}
+float target::getMoney(){
+    return money;
+}
+//player constructor function
+player::player(){
     
+}
+//getter functions for player class 
+int player::getMoney(){
+    return money;
+}
+float player::getHashpower(){
+    return hashpower;
+}
+int* player::getHardware(){
+    return hardware;
 }
